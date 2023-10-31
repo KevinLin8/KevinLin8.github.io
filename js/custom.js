c034a2bae7c00bf983f923e04d8ec74dd637f78d
jianlin <2367884500@qq.com>
2023-10-31 14:30:06 +0800

    Site updated: 2023-10-31 14:30:03

--- js/custom.js
+++ js/custom.js
@@ -22,9 +22,19 @@ function TimeCalculation () {
     ); // 计算秒差值
     return time
 }
-function updateDom(){
 const footer = document.querySelector('footer')
-footer.innerHTML = `
-    <div>此站已运行： <span>${TimeCalculation().diffInDays}</span>天<span>${TimeCalculation().diffInHours}</span>时<span>${TimeCalculation().diffInMinutes}</span>分<span>${TimeCalculation().diffInSeconds}</span>秒</div>`
+footer.innerHTML = ''
+let my_div = document.createElement('div')
+my_div.innerHTML = `
+    <div>本站已运行： <span>${TimeCalculation().diffInDays}</span> 天 <span>${TimeCalculation().diffInHours}</span> 时 <span>${TimeCalculation().diffInMinutes}</span> 分 <span>${TimeCalculation().diffInSeconds}</span> 秒 </div>`
+footer.appendChild(my_div);
+
+function updateDom(){
+    if(my_div.children[0].children.length){
+        my_div.children[0].children[0].textContent = TimeCalculation().diffInDays
+        my_div.children[0].children[1].textContent = TimeCalculation().diffInHours
+        my_div.children[0].children[2].textContent = TimeCalculation().diffInMinutes
+        my_div.children[0].children[3].textContent = TimeCalculation().diffInSeconds
+    }
 }
 setInterval(updateDom,1000)
